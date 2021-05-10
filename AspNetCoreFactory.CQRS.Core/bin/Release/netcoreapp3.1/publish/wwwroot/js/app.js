d621af844e28ee1fd5684010893504c35cb41b3f
Raju Nadimpalli <raju@srinadimpalli.com>
2021-05-10 16:38:19 -0400

    Initial version

---
+++ AspNetCoreFactory.CQRS.Core/bin/Release/netcoreapp3.1/publish/wwwroot/js/app.js
@@ -0,0 +1,8 @@
+﻿
+$(function () {
+
+   // Bootstrap 'disabled' class does not stop click events.
+   // This will prevent this.
+
+   $('.disabled').on('click', function (e) { e.preventDefault(); return false; });
+});
